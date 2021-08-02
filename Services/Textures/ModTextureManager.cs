67ca8eba8b136418042d7ba9311ba665b1a579c5
Pbone3 <pbone785@gmail.com>
2021-08-02 17:01:14 -0500

    More stuff

---
+++ Services/Textures/ModTextureManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace PboneLib.Services.Textures
+{
+    public abstract class ModTextureManager : ModSystem
+    {
+        public Dictionary<string, IAsset> CachedAssets;
+
+        public abstract Dictionary<string, string> GetImmediateTextures();
+        public abstract Dictionary<string, string> GetOtherTextures();
+
+        public override void Load()
+        {
+            base.Load();
+
+            foreach (KeyValuePair<string, string> s in GetImmediateTextures())
+            {
+                CachedAssets.Add(s.Key, ModContent.Request<Texture2D>(s.Value, AssetRequestMode.ImmediateLoad));
+            }
+        }
+
+        public IAsset GetAsset(string name)
+        {
+            if (CachedAssets.TryGetValue(name, out IAsset asset))
+                return asset;
+
+            CachedAssets.Add(name, ModContent.Request<Texture2D>(GetOtherTextures()[name]));
+            return CachedAssets[name];
+        }
+    }
+}

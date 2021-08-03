fa32ef73be1002475c9982144da352cfbf042fe4
Pbone3 <pbone785@gmail.com>
2021-08-03 04:06:35 -0500

    I'm stupid

---
+++ CustomLoading/Implementations/Content/PWall.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace PboneLib.CustomLoading.Implementations
+{
+    public abstract class PWall : ModWall, ICustomLoadable
+    {
+        public virtual bool LoadCondition() => true;
+    }
+}

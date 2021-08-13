c832a2869b6d4670301d5eb318e50be8ac9a19bd
Pbone3 <pbone785@gmail.com>
2021-08-12 23:38:29 -0500

    toml localization loader fix

--- CustomLoading/Localization/Parsers/TomlFileParser.cs
+++ CustomLoading/Localization/Parsers/TomlFileParser.cs
@@ -14,18 +14,42 @@ namespace PboneLib.CustomLoading.Localization.Parsers
             Dictionary<string, ModTranslation> dictionary = new Dictionary<string, ModTranslation>();
             foreach (KeyValuePair<string, TomlValue> kvp in document.Entries)
             {
-                string key = kvp.Key;
-                string value = kvp.Value.StringValue;
-                value = value.Replace("\\n", "\n");
+                GetValues(kvp.Key, kvp.Value, out IDictionary<string, string> values);
 
-                if (!dictionary.TryGetValue(key, out var translation))
+                foreach (KeyValuePair<string, string> tr in values)
                 {
-                    translation = (dictionary[key] = TModLocalizationLoader.CreateTranslation(mod, key));
+                    string key = tr.Key;
+                    string value = tr.Value;
+                    value = value.Replace("\\n", "\n");
+
+                    if (!dictionary.TryGetValue(key, out var translation))
+                    {
+                        translation = (dictionary[key] = TModLocalizationLoader.CreateTranslation(mod, key));
+                    }
+                    translation.AddTranslation(culture, value);
                 }
-                translation.AddTranslation(culture, value);
             }
 
             return dictionary;
+        }
+
+        public void GetValues(string key, TomlValue toml, out IDictionary<string, string> values)
+        {
+            if (toml is TomlTable table)
+            {
+                values = new Dictionary<string, string>();
+
+                foreach (KeyValuePair<string, TomlValue> kvp in table.Entries)
+                {
+                    GetValues(kvp.Key, kvp.Value, out values);
+                }
+
+                return;
+            }
+
+            values = new Dictionary<string, string> {
+                [key] = toml.StringValue
+            };
         }
     }
 }

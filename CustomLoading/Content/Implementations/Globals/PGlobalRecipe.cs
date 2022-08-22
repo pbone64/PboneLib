19baf6e27da518a70f43cef9da20c8be8a80a109
Pbone3 <pbone785@gmail.com>
2022-08-22 04:11:16 -0500

    aaaa

--- CustomLoading/Content/Implementations/Globals/PGlobalRecipe.cs
+++
@@ -1,9 +0,0 @@
-﻿using Terraria.ModLoader;
-
-namespace PboneLib.CustomLoading.Content.Implementations.Globals
-{
-    public abstract class PGlobalRecipe : GlobalRecipe, ICustomLoadable
-    {
-        public virtual bool LoadCondition() => true;
-    }
-}

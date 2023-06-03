c2cb3aa708058f68a58aeb362b3f051a735a9df0
Rijam <jacobrmoreno@hotmail.com>
2023-06-02 18:29:08 -0600

    1.4.4 Update 1
    Custom localization is even more broken with this update

--- CustomLoading/Localization/ILocalizationFileParser.cs
+++ CustomLoading/Localization/ILocalizationFileParser.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace PboneLib.CustomLoading.Localization
 {
     public interface ILocalizationFileParser
     {
-        IDictionary<string, ModTranslation> ParseStream(Mod mod, string culture, Stream stream)
+        IDictionary<string, LocalizedText> ParseStream(Mod mod, string culture, Stream stream)
         {
             using StreamReader reader = new StreamReader(stream);
             return ParseText(mod, culture, reader.ReadToEnd());
         }
 
-        IDictionary<string, ModTranslation> ParseText(Mod mod, string culture, string text);
+        IDictionary<string, LocalizedText> ParseText(Mod mod, string culture, string text);
     }
 }
